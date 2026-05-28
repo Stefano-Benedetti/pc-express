97cfdf390ba692814eaa3f12231711898585d0cc
Simone <enomis000000@gmail.com>
2026-05-28 18:07:46 +0200

    aggiunta pagina per visualizzare la singola vendita effettuata

--- src/main/java/it/progettosiw/pcexpress/controller/SaleController.java
+++ src/main/java/it/progettosiw/pcexpress/controller/SaleController.java
@@ -1,10 +1,15 @@
 package it.progettosiw.pcexpress.controller;
 
+import it.progettosiw.pcexpress.model.Sale;
 import it.progettosiw.pcexpress.service.PCService;
 import it.progettosiw.pcexpress.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SaleController {
@@ -16,9 +21,16 @@ public class SaleController {
         this.saleService = saleService;
     }
 
-    @PostMapping("/sale/makeNewSale/item")
-    public String createSaleForSingleItem(){
-        return "";
+    @PostMapping("/sale/makeNewSale")
+    public String createSaleForSingleItem(@RequestParam Long pcId, @RequestParam Integer quantity, Model model){
+        Sale sale = saleService.createSaleFromPC(pcId, quantity);
+        return "redirect:/sale/single_sale/"+sale.getId().toString();
+    }
+
+    @GetMapping("/sale/single_sale/{sale_id}")
+    public String showForSingleSale(@PathVariable("sale_id") Long sale_id, Model model){
+        model.addAttribute("sale", saleService.getSaleById(sale_id));
+        return "/sale/single_sale";
     }
 }
 
--- src/main/java/it/progettosiw/pcexpress/service/SaleService.java
+++ src/main/java/it/progettosiw/pcexpress/service/SaleService.java
@@ -1,19 +1,71 @@
 package it.progettosiw.pcexpress.service;
 
+import it.progettosiw.pcexpress.model.*;
+import it.progettosiw.pcexpress.repository.PCRepository;
 import it.progettosiw.pcexpress.repository.SaleRepository;
 import it.progettosiw.pcexpress.repository.SoldItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SaleService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CartService.class);
+
+
+    private UserService userService;
     private SaleRepository saleRepository;
     private SoldItemRepository soldItemRepository;
+    private PCRepository pcRepository;
 
-    public SaleService(SaleRepository saleRepository, SoldItemRepository soldItemRepository) {
+    public SaleService(SaleRepository saleRepository, SoldItemRepository soldItemRepository, PCRepository pcRepository, UserService userService) {
         this.saleRepository = saleRepository;
         this.soldItemRepository = soldItemRepository;
+        this.pcRepository=pcRepository;
+        this.userService = userService;
     }
 
+    public Sale getSaleById(Long sale_id){
+        Optional<Sale> optSale = saleRepository.findById(sale_id);
+        if(!optSale.isPresent()){
+            logger.error("sale non presente");
+            return null;
+        }
+        return optSale.get();
+    }
+
+    public SoldItem createSoldItem(Long pc_id, Integer quantity){
+        Optional<PC> optPC = pcRepository.findById(pc_id);
+        if(!optPC.isPresent()){
+            logger.error("pc non trovatoooooo durante sale");
+            return null;
+        }
+        PC pc = optPC.get();
+
+        return new SoldItem(quantity, pc.getPrezzo(), pc);
+    }
+
+    //è chiamato quando si acquista dal carrello
+    public void createSale(Cart cart){
+
+    }
+    //è chaiamto quando si acquista dalla pagina di un pc
+    public Sale createSaleFromPC(Long pc_id, Integer quantity){
+        SoldItem soldItem = createSoldItem(pc_id, quantity);
+        Float totalSoldItemPrice = soldItem.getPaidMoney()*quantity;
+
+        List<SoldItem> soldItems = new ArrayList<>();
+        soldItems.add(soldItem);
+
+        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, userService.getCurrentUser());
+        saleRepository.save(sale);
+        return sale;
+    }
 
 }
