8f70ed76768a8f3f3848cf349eb2236623311364
Simone <enomis000000@gmail.com>
2026-06-24 16:05:03 +0200

    quasi del tutto rifattorizzati i metodi per creare la Sale

--- src/main/java/it/progettosiw/pcexpress/service/SaleService.java
+++ src/main/java/it/progettosiw/pcexpress/service/SaleService.java
@@ -64,6 +64,148 @@ public class SaleService {
         return userService.getCurrentUser().getPurchases();
     }
 
+//    @Transactional(readOnly = true)
+//    public SoldItem createSoldItem(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
+//        if (quantity<1)
+//            throw new NonPositiveQuantityException();
+//        Optional<PC> optPC = pcRepository.findById(pc_id);
+//        if(!optPC.isPresent()){
+//            logger.error("pc non trovatoooooo durante sale");
+//            return null;
+//        }
+//        PC pc = optPC.get();
+//
+//        return new SoldItem(quantity, pc.getPrezzo(), pc);
+//    }
+//
+//    //è chaiamato quando si acquista dalla pagina di un pc
+//    @Transactional(isolation = Isolation.SERIALIZABLE)  //necessario a causa della riduzione della disponibilità
+//    public Sale createSaleFromPC(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
+//        SoldItem soldItem = createSoldItem(pc_id, quantity);
+//        Float totalSoldItemPrice = soldItem.getPaidMoney()*quantity;
+//
+//        soldItem.getPc().reduceAvailability(quantity);
+//
+//        List<SoldItem> soldItems = new ArrayList<>();
+//        soldItems.add(soldItem);
+//
+//        User currentUser = userService.getCurrentUser();
+//
+//        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
+//
+//        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
+//
+//        saleRepository.save(sale);
+//        return sale;
+//    }
+//
+//    //è chiamato quando si acquista dal carrello
+//    @Transactional(isolation = Isolation.SERIALIZABLE)
+//    public Sale createSaleFromCart() throws NonPositiveQuantityException, EmptyCartDuringSaleCreationException{
+//        User currentUser = userService.getCurrentUser();
+//        Cart currentCart = currentUser.getCart();
+//
+//        if (currentCart.getCartItems().isEmpty()){
+//            throw new EmptyCartDuringSaleCreationException();
+//        }
+//
+//        List<SoldItem> soldItems = new ArrayList<>();
+//        Float totalSoldItemPrice = 0F;
+//
+//        for(CartItem cartItem : currentCart.getCartItems()){
+//            Integer quantity = cartItem.getQuantity();
+//
+//            if (quantity<1)
+//                throw new NonPositiveQuantityException();
+//
+//            PC pc = cartItem.getPc();
+//            pc.reduceAvailability(quantity);
+//
+//            SoldItem soldItem = new SoldItem(quantity, pc.getPrezzo(), pc);
+//
+//            totalSoldItemPrice += soldItem.getPaidMoney()*quantity;
+//            soldItems.add(soldItem);
+//        }
+//
+//        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
+//
+//        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
+//
+//        saleRepository.save(sale);
+//        cartService.emptyCurrentUserCart();
+//        return sale;
+//    }
+
+
+
+//    @Transactional(readOnly = true)
+//    public SoldItem createSoldItem(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
+//        if (quantity<1)
+//            throw new NonPositiveQuantityException();
+//        Optional<PC> optPC = pcRepository.findById(pc_id);
+//        if(!optPC.isPresent()){
+//            logger.error("pc non trovatoooooo durante sale");
+//            return null;
+//        }
+//        PC pc = optPC.get();
+//
+//        return new SoldItem(quantity, pc.getPrezzo(), pc);
+//    }
+//
+//
+//    //è chaiamato quando si acquista dalla pagina di un pc
+//    @Transactional(isolation = Isolation.SERIALIZABLE)  //necessario a causa della riduzione della disponibilità
+//    public Sale createSaleFromPC(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
+//        User currentUser = userService.getCurrentUser();
+//        List<SoldItem> soldItems = new ArrayList<>();
+//        Float totalSoldItemPrice = 0F;
+//
+//        SoldItem soldItem = createSoldItem(pc_id, quantity);
+//        totalSoldItemPrice = soldItem.getPaidMoney()*quantity;
+//        soldItems.add(soldItem);
+//
+//        soldItem.getPc().reduceAvailability(quantity);
+//
+//        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
+//        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
+//
+//        saleRepository.save(sale);
+//        return sale;
+//    }
+//
+//    //è chiamato quando si acquista dal carrello
+//    @Transactional(isolation = Isolation.SERIALIZABLE)
+//    public Sale createSaleFromCart() throws NonPositiveQuantityException, EmptyCartDuringSaleCreationException{
+//        User currentUser = userService.getCurrentUser();
+//        Cart currentCart = currentUser.getCart();
+//        if (currentCart.getCartItems().isEmpty()){
+//            throw new EmptyCartDuringSaleCreationException();
+//        }
+//        List<SoldItem> soldItems = new ArrayList<>();
+//        Float totalSoldItemPrice = 0F;
+//
+//        for(CartItem cartItem : currentCart.getCartItems()){
+//            Integer quantity = cartItem.getQuantity();
+//            if (quantity<1)
+//                throw new NonPositiveQuantityException();
+//            PC pc = cartItem.getPc();
+//            SoldItem soldItem = new SoldItem(quantity, pc.getPrezzo(), pc);
+//            totalSoldItemPrice += soldItem.getPaidMoney()*quantity;
+//            soldItems.add(soldItem);
+//
+//            pc.reduceAvailability(quantity);
+//        }
+//
+//        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
+//        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
+//
+//        saleRepository.save(sale);
+//        cartService.emptyCurrentUserCart();
+//        return sale;
+//    }
+
+
+
     @Transactional(readOnly = true)
     public SoldItem createSoldItem(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
         if (quantity<1)
@@ -78,60 +220,50 @@ public class SaleService {
         return new SoldItem(quantity, pc.getPrezzo(), pc);
     }
 
-    //è chaiamato quando si acquista dalla pagina di un pc
+    private SoldItem createSoldItem(CartItem cartItem) throws NonPositiveQuantityException {
+        Integer quantity = cartItem.getQuantity();
+        if (quantity < 1)
+            throw new NonPositiveQuantityException();
+        return new SoldItem(quantity, cartItem.getPc().getPrezzo(), cartItem.getPc());
+    }
+
     @Transactional(isolation = Isolation.SERIALIZABLE)  //necessario a causa della riduzione della disponibilità
-    public Sale createSaleFromPC(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
-        SoldItem soldItem = createSoldItem(pc_id, quantity);
-        Float totalSoldItemPrice = soldItem.getPaidMoney()*quantity;
-
-        soldItem.getPc().reduceAvailability(quantity);
-
-        List<SoldItem> soldItems = new ArrayList<>();
-        soldItems.add(soldItem);
-
-        User currentUser = userService.getCurrentUser();
-
-        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
-
-        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
-
+    public Sale createAndSaveSale(User user, List<SoldItem> soldItems){
+        Float totalPrice = 0F;
+        for (SoldItem item : soldItems){
+            Integer quantity = item.getQuantity();
+            totalPrice += item.getPaidMoney()*quantity;
+            item.getPc().reduceAvailability(quantity);
+        }
+        Sale sale = new Sale(LocalDateTime.now(), totalPrice, soldItems, user);
+        user.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
         saleRepository.save(sale);
         return sale;
     }
 
+    //è chaiamato quando si acquista dalla pagina di un pc
+    public Sale createSaleFromPC(Long pc_id, Integer quantity) throws NonPositiveQuantityException{
+        User currentUser = userService.getCurrentUser();
+        SoldItem soldItem = createSoldItem(pc_id, quantity);
+        return createAndSaveSale(currentUser, List.of(soldItem));
+    }
+
     //è chiamato quando si acquista dal carrello
-    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Transactional //come gestisco il fatto che il carrello viene svuotato??????????
     public Sale createSaleFromCart() throws NonPositiveQuantityException, EmptyCartDuringSaleCreationException{
         User currentUser = userService.getCurrentUser();
         Cart currentCart = currentUser.getCart();
-
         if (currentCart.getCartItems().isEmpty()){
             throw new EmptyCartDuringSaleCreationException();
         }
-
         List<SoldItem> soldItems = new ArrayList<>();
-        Float totalSoldItemPrice = 0F;
 
         for(CartItem cartItem : currentCart.getCartItems()){
-            Integer quantity = cartItem.getQuantity();
-
-            if (quantity<1)
-                throw new NonPositiveQuantityException();
-
-            PC pc = cartItem.getPc();
-            pc.reduceAvailability(quantity);
-
-            SoldItem soldItem = new SoldItem(quantity, pc.getPrezzo(), pc);
-
-            totalSoldItemPrice += soldItem.getPaidMoney()*quantity;
+            SoldItem soldItem = createSoldItem(cartItem);
             soldItems.add(soldItem);
         }
 
-        Sale sale = new Sale(LocalDateTime.now(), totalSoldItemPrice, soldItems, currentUser);
-
-        currentUser.getPurchases().add(sale);  //serve a mantenere aggiornato lo user in locale
-
-        saleRepository.save(sale);
+        Sale sale = createAndSaveSale(currentUser, soldItems);
         cartService.emptyCurrentUserCart();
         return sale;
     }
